74cd59ecfeb735b10fbd976ca2e3f160d37efffa
boa50 <bruno.albuquerque50@gmail.com>
2022-07-10 18:25:47 -0300

    Added all the visualizations to the R markdown file

--- scripts.R
+++ scripts.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(janitor)
+library(scales)
 
 # set working directory
 # putting this way to become easier to manage the variables
@@ -98,9 +99,14 @@ df_enrollment %>%
   filter(income_group %in% c('Brazil', 'Upper middle income') & 
            indicator_code == 'SE.SEC.NENR' & year == '2019') %>%
   ggplot(aes(x=enrollment)) + 
+  theme_minimal() +
   geom_histogram(binwidth=5, fill='#80b1d3') +
   geom_vline(aes(xintercept=brazil_value), color='#757575', size=1, linetype='dashed') +
-  geom_text(aes(x=brazil_value+2.5, y=-.2, label='Brazil'))
+  geom_text(aes(x=brazil_value+2, y=-.3, label='Brazil')) +
+  scale_x_continuous(labels=function(x) paste(x, '%', sep='')) +
+  scale_y_continuous(breaks = pretty_breaks()) +
+  labs(title='Enrollment in the Secondary education of the Upper middle income group on 2019', 
+       x='Enrollment', y='Number of Countries')
 
 # checking the investment in education
 # getting money invested in education based on GDP (https://data.oecd.org/gdp/gross-domestic-product-gdp.htm)
@@ -108,21 +114,41 @@ df_gdp_spent_percent <- df_cleaned %>%
   filter(indicator_code  == 'SE.XPD.TOTL.GD.ZS') %>%
   # pivoting table to make it easier to plot
   pivot_longer(x2010:x2019, names_to='year', values_to='gdp_percent') %>%
+  # cleaning year values to make it easier to use as label
+  mutate(year=gsub('x', '', .$year))
+
+# above of 20% of the data has NA values
+# because of this we will use the strategy to fill years with previous values or next values
+print(
+  paste(
+    'Percentage of NA values on gdp_percent column: ',
+    trunc(sum(is.na(df_gdp_spent_percent$gdp_percent)) / nrow(df_gdp_spent_percent)*100), 
+    '%', 
+    sep=''
+  )
+)
+
+df_gdp_spent_percent <- df_gdp_spent_percent %>%
   group_by(country_code, indicator_code) %>%
   fill(gdp_percent, .direction='downup') %>%
   # filtering to get only the countries with at least 1 not NA value for each indicator
   filter(!is.na(gdp_percent))
 
-df_gdp_spent_grouped <- df_gdp_spent_percent %>%
+df_gdp_spent_percent %>%
   group_by(income_group, indicator_code, year) %>%
   summarise(mean=mean(gdp_percent), sd=sd(gdp_percent)) %>%
-# cleaning year values to make it easier to use as label
-  mutate(year=gsub('x', '', year))
-
-
-ggplot(df_gdp_spent_grouped, aes(x=year, y=mean, group=income_group)) +
-  geom_line(aes(color=income_group)) +
-  expand_limits(y = 0)
+  ggplot(aes(x=year, y=mean, group=income_group)) +
+  theme_minimal() +
+  geom_line(aes(color=income_group), size=1) +
+  geom_ribbon(aes(ymin=mean - sd, ymax=mean + sd, fill=income_group), alpha=.15) +
+  expand_limits(y = 0) +
+  # changing the order of the legend values
+  scale_fill_brewer(breaks=legend_order, palette=color_scale) +
+  scale_colour_brewer(breaks=legend_order, palette=color_scale) +
+  # formatting the y labels to include a percent sign
+  scale_y_continuous(labels=function(y) paste(y, '%', sep='')) +
+  labs(title='Average GDP spent in education', 
+       x='Year', y='GDP spent', color='Income Group', fill='Income Group')
 
 
 # getting GDP per capita changes over time
@@ -130,21 +156,13 @@ df_gdp <- df_cleaned %>%
   filter(indicator_code  == 'NY.GDP.PCAP.KD') %>%
   # pivoting table to make it easier to plot
   pivot_longer(x2010:x2019, names_to='year', values_to='gdp_per_capita') %>%
+  # cleaning year values to make it easier to use as label
+  mutate(year=gsub('x', '', .$year)) %>%
   group_by(country_code, indicator_code) %>%
   fill(gdp_per_capita, .direction='downup') %>%
   # filtering to get only the countries with at least 1 not NA value for each indicator
   filter(!is.na(gdp_per_capita))
 
-df_gdp_grouped <- df_gdp %>%
-  group_by(income_group, indicator_code, year) %>%
-  summarise(mean=mean(gdp_per_capita), sd=sd(gdp_per_capita)) %>%
-  # cleaning year values to make it easier to use as label
-  mutate(year=gsub('x', '', year))
-  
-ggplot(df_gdp_grouped, aes(x=year, y=mean, group=income_group)) +
-  geom_line(aes(color=income_group)) +
-  expand_limits(y = 0)
-  
 df_gdp_spent_per_capita <- df_gdp_spent_percent %>%
   # joining with gdp data to be able to get the gpd expenditure per capita
   merge(df_gdp, by=c('country_code', 'year')) %>%
@@ -154,19 +172,23 @@ df_gdp_spent_per_capita <- df_gdp_spent_percent %>%
   mutate(
     gdp_spent_per_capita=(gdp_percent/100) * gdp_per_capita, 
     year=gsub('x', '', year))
-  
-df_gdp_spent_per_capita_grouped <- df_gdp_spent_per_capita %>%
+
+df_gdp_spent_per_capita %>%
   group_by(income_group, year) %>%
-  summarise(mean=mean(gdp_spent_per_capita), sd=sd(gdp_spent_per_capita)) 
-
-
-ggplot(df_gdp_spent_per_capita_grouped, aes(x=year, y=mean, group=income_group)) +
+  summarise(mean=mean(gdp_spent_per_capita), sd=sd(gdp_spent_per_capita)) %>%
+  ggplot(aes(x=year, y=mean, group=income_group)) +
+  theme_minimal() +
   geom_line(aes(color=income_group), size=1) +
   geom_ribbon(aes(ymin=mean - sd, ymax=mean + sd, fill=income_group), alpha=.15) +
   expand_limits(y = 0) +
-  # changing the order of the legend values and setting default colors
+  # changing the order of the legend values
   scale_fill_brewer(breaks=legend_order, palette=color_scale) +
-  scale_colour_brewer(breaks=legend_order, palette=color_scale)
+  scale_colour_brewer(breaks=legend_order, palette=color_scale) +
+  # formatting the y labels to include a percent sign
+  scale_y_continuous(labels=function(y) paste('$', y, sep='')) +
+  labs(title='Average GDP per capita spent in education', 
+       caption='The values are based on 2015 US$',
+       x='Year', y='GDP per capita spent', color='Income Group', fill='Income Group')
 
 # let's compare how far we are from the majority of high income countries
 brazil_value_gdp_per_capita <- df_gdp_spent_per_capita %>%
@@ -176,27 +198,51 @@ brazil_value_gdp_per_capita <- brazil_value_gdp_per_capita$gdp_spent_per_capita
 df_gdp_spent_per_capita %>%
   filter(income_group %in% c('Brazil', 'High income') & year == '2019') %>%
   ggplot(aes(x=gdp_spent_per_capita)) + 
+  theme_minimal() +
   geom_histogram(binwidth=200, fill='#80b1d3') +
-  geom_vline(aes(xintercept=brazil_value_gdp_per_capita), color='#757575', size=1, linetype='dashed') +
-  geom_text(aes(x=brazil_value_gdp_per_capita+250, y=-.2, label='Brazil'))
+  geom_vline(aes(xintercept=brazil_value), color='#757575', size=1, linetype='dashed') +
+  geom_text(aes(x=brazil_value+200, y=-.3, label='Brazil')) +
+  scale_x_continuous(labels=function(x) paste('$', x, sep='')) +
+  scale_y_continuous(breaks = pretty_breaks()) +
+  labs(title='GDP per capita spent in education of the High income group on 2019 and Brazil', 
+       caption='The values are based on 2015 US$',
+       x='GDP per capita spent', y='Number of Countries')
+
 
 
 # getting data on children in employment 7 - 14 year
 # SL.TLF.0714.ZS
-df_children_employment <- df_cleaned %>%
-  filter(indicator_code  == 'SL.TLF.0714.ZS') %>%
+df_children_employment <- df_cleaned %>% filter(indicator_code  == 'SL.TLF.0714.ZS')
+
+# Almost 85% of the data is NA
+# because of this we will use the strategy to get only the last year from the last five
+print(
+  paste(
+    'Percentage of NA values on years columns: ',
+    trunc(sum(rowSums(is.na(df_children_employment))) / (nrow(df_children_employment)*10)*100), 
+    '%', 
+    sep=''
+  )
+)
+
+df_children_employment <- df_children_employment %>%
   # getting only the last year from the last five because there is a lot of NA values
   mutate(children_employment=do.call(coalesce, rev(across(x2015:x2019)))) %>%
   select(c(country_code, income_group, children_employment)) %>%
   filter(!is.na(children_employment))
 
 ggplot(df_children_employment, aes(x=income_group, y=children_employment)) + 
+  theme_minimal() +
   geom_boxplot(aes(fill=income_group)) +
   # removing the legend as it is not necessary on this chart
   theme(legend.position='none') +
   # changing the order of the legend values and setting default colors
   scale_x_discrete(limits=legend_order) +
-  scale_fill_brewer(palette=color_scale)
+  scale_y_continuous(labels=function(y) paste(y, '%', sep='')) +
+  scale_fill_brewer(palette=color_scale) +
+  labs(title='Percentage of children employment per income group', 
+       caption='Only children with 7 - 14 years old were considered',
+       x='Income Group', y='Children Employment')
 
 
 # getting data about poverty !caution
@@ -209,9 +255,24 @@ ggplot(df_children_employment, aes(x=income_group, y=children_employment)) +
 poverty_indicators <- c('SI.DST.FRST.20', 'SI.DST.02ND.20' , 'SI.DST.03RD.20', 'SI.DST.04TH.20', 'SI.DST.05TH.20')
 
 df_poverty <- df_cleaned %>%
-  filter(indicator_code  %in% poverty_indicators) %>%
+  filter(indicator_code %in% poverty_indicators) %>%
   # pivoting table to make it easier to plot
   pivot_longer(x2010:x2019, names_to='year', values_to='income_share') %>%
+  # cleaning year values to make it easier to use as label
+  mutate(year=gsub('x', '', year))
+
+# Almost 50% of the data has NA values
+# because of this we will use the strategy to fill years with previous values or next values
+print(
+  paste(
+    'Percentage of NA values on income_share column: ',
+    trunc(sum(is.na(df_poverty$income_share)) / nrow(df_poverty)*100), 
+    '%', 
+    sep=''
+  )
+)
+  
+df_poverty <- df_poverty %>%
   group_by(country_code, indicator_code) %>%
   # filling NA values so that each year could have the same number of countries
   fill(income_share, .direction='downup') %>%
@@ -220,16 +281,35 @@ df_poverty <- df_cleaned %>%
 df_poverty_grouped <- df_poverty %>%
   group_by(income_group, indicator_code, year) %>%
   summarise(mean=mean(income_share), sd=sd(income_share)) %>%
-  # cleaning year values to make it easier to use as label
-  mutate(year=gsub('x', '', year)) %>%
   # getting the difference of values between years per group
   mutate(years_difference=abs(mean - lag(mean)))
 
 # as the difference of values between years are so small, we are
 # comparing values based only on the last year
-print(max(df_poverty_grouped$years_difference, na.rm=TRUE))
-ggplot(df_poverty_grouped, aes(x=years_difference)) + 
-  geom_histogram(fill='#80b1d3')
+print(
+  paste(
+    'Maximum difference of values between years: ',
+    max(df_poverty_grouped$years_difference, na.rm=TRUE), 
+    '%', 
+    sep=''
+  )
+)
+
+ggplot(df_poverty_grouped, aes(x=years_difference)) +
+  theme_minimal() +
+  geom_histogram(binwidth=.025, fill='#80b1d3') +
+  scale_x_continuous(labels=function(x) paste(x, '%', sep='')) +
+  labs(title='Difference of income share between years', 
+       x='Difference between years', y='Number of Values')
+
+# setting representative labels to the indicators
+poverty_indicators_labels <- c(
+  'Highest 20%',
+  'Fourth 20%',
+  'Third 20%',
+  'Second 20%',
+  'Lowest 20%'
+)
 
 df_poverty_grouped %>%
   filter(year == '2019') %>%
@@ -237,12 +317,15 @@ df_poverty_grouped %>%
     # changing the order of the legend values
     fill=factor(indicator_code, levels=rev(poverty_indicators)),
     y=mean, x=income_group)) + 
+  theme_minimal() +
   geom_bar(position=position_dodge(), stat='identity') +
   # inserting the standard deviation bars https://bioinformatics.stackexchange.com/questions/11222/stacked-bargraph-with-error-bars
   geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=0.3, position=position_dodge(0.9), color='#757575') +
   # setting other colors to differentiate from previous charts
   # got this custom pallet based on https://colors.dopely.top/palette-generator/ToL9uxh2Egw
-  scale_fill_manual(values=c('#82AC85','#A9C7AC','#BFD8C4','#E2CFC9','#D1ACA5')) + 
+  scale_fill_manual(values=c('#82AC85','#A9C7AC','#BFD8C4','#E2CFC9','#D1ACA5'), 
+                    labels=poverty_indicators_labels) + 
   scale_x_discrete(limits=legend_order) +
-  labs(fill='indicator_code') +
-  theme_classic()
+  scale_y_continuous(labels=function(y) paste(y, '%', sep='')) +
+  labs(title='Percentage of income share per Population Group and Income Group on 2019', 
+       x='Income Group', y='Income Share', fill='Population Share Group')
